4fbb29748cbce5c72c9d32821c74ec93c7fcc8f3
carolmoises <carol.moises@gmail.com>
2021-04-20 19:56:07 -0300

    Gestão Academica app index

--- gestaoacademicaapp/views.py
+++ gestaoacademicaapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Disciplina, Matricula
 
-# Create your views here.
+def listaMatriculas(request):
+    
+    listadisciplina = Matricula.objects.all
+
+    return render(request, 'gestaoacademicaapp/index.html', {'disciplinas':listadisciplina})
+
+def fazerMatricula(request, id):
+    matriculardisciplina = get_object_or_404(Matricula, pk=id)
+
+    if(matriculardisciplina.status == 'prevista'):
+        matriculardisciplina.status = 'matriculado'
+
+    matriculardisciplina.save()
+
+    return redirect('/')
